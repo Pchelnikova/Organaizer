ae5339921463e0d6b56d9dcc3007c443869113f3
Irina <pchelnikova23@gmail.com>
2018-06-26 22:47:10 +0300

    Update Ninject  + Rename InterfaceBLL

--- WcfService/App_Start/Ninject.Web.Common.cs
+++ WcfService/App_Start/Ninject.Web.Common.cs
@@ -61,7 +61,7 @@ namespace WcfService.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-
+           
         }        
     }
 }
